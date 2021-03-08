ce0f16cf13fef29b4c8770b6e6e5612bb3d7b029
seichris <cerujurasa@gmail.com>
2021-03-08 19:33:37 +0100

    fix type testing error

--- test/testUtils.ts
+++ test/testUtils.ts
@@ -3,7 +3,11 @@ import { render } from "@testing-library/react";
 // import { TranslationProvider } from "my-i18n-lib"
 // import defaultStrings from "i18n/en-x-default"
 
-const Providers = ({ children }) => {
+type Props = {
+  children?: ReactNode;
+};
+
+const Providers = ({ children }: Props) => {
   return children;
   // return (
   //   <ThemeProvider theme="light">
